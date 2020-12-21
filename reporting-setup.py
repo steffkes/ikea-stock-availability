0af7f6bd93c7a3a41e84e2630762990fb9b05bc4
steffkes <stefan@gelungen.es>
2020-12-21 22:30:02 +0100

    switch to compressed files

--- reporting-setup.py
+++ reporting-setup.py
@@ -7,9 +7,9 @@ def flatten(stock):
    data["stock"] = stock["RetailItemAvailability"]["AvailableStock"]["$"]
    return pd.Series(data)
 
-data = pd.read_json("/data/combined.jsonl", lines=True)
+data = pd.read_json("/tmp/combined.jsonl.gz", lines=True, compression="gzip")
 data = data.join(data["StockAvailability"].apply(flatten))
 data = data.drop(columns=["StockAvailability"])
 data["store_id"] = data["store_id"].astype(str)
 data["article_id"] = data["article_id"].astype(str)
-data.to_json("/data/stock.jsonl.gz", compression="gzip", orient="records", lines=True)
+data.to_json("/tmp/stock.jsonl.gz", compression="gzip", orient="records", lines=True)
