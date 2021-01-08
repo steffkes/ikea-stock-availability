2d6c3e3669753cc94c3c9cc767c1320dab348731
steffkes <stefan@gelungen.es>
2021-01-08 18:20:07 +0100

    visualize restock_date as difference in days

--- tmp-01.py
+++ tmp-01.py
@@ -3,9 +3,22 @@ import pandas as pd
 import altair as alt
 
 
+def compute_diff(row):
+    if row["restock_date"] is None or row["restock_date"] == pd.np.nan:
+        return None
+
+    else:
+        return (row["restock_date"] - row["_fetched_at"]).days
+
+
 @st.cache
 def load_data():
-    data = pd.read_json("tmp/70277957-328-restock.jsonl.gz", lines=True, compression="gzip")
+    data = pd.read_json(
+        "tmp/70277957-328-restock.jsonl.gz", lines=True, compression="gzip"
+    )
+    data = data.replace({pd.np.nan: None})
+    data["restock_date"] = pd.to_datetime(data["restock_date"], utc=True)
+    data["restock_diff"] = data.apply(compute_diff, axis=1)
     return data
 
 
@@ -17,28 +30,28 @@ if st.checkbox("Show raw data"):
 st.altair_chart(
     alt.layer(
         alt.Chart(df)
-        .mark_line(color="steelblue", opacity=0.25)
+        .mark_line(color="steelblue", opacity=0.5)
         .encode(
-            x=alt.X(
-              "_fetched_at",
-              axis=alt.Axis(format='%d.%m')
-            ),
-            y=alt.Y(
-              "available_stock",
-              axis=alt.Axis(titleColor="steelblue")
-            ),
+            x=alt.X("_fetched_at", axis=alt.Axis(format="%d.%m")),
+            y=alt.Y("available_stock", axis=alt.Axis(titleColor="steelblue")),
         ),
         alt.Chart(df)
-        .mark_line(color="red")
+        .mark_line(color="red", opacity=0.5)
         .encode(
             x="_fetched_at",
             y=alt.Y(
-              "restock_date:T",
-              axis=alt.Axis(format='%d.%m', grid=True, titleColor="red")
+                "restock_diff",
+                axis=alt.Axis(titleColor="red", title="restock in x days"),
             ),
-            tooltip=["_fetched_at", "store_id", "restock_date:T", "available_stock"],
+            tooltip=[
+                "_fetched_at",
+                "store_id",
+                "restock_date",
+                "restock_diff",
+                "available_stock",
+            ],
         )
-        .interactive()
-    ).resolve_scale(y='independent'),
+        .interactive(),
+    ).resolve_scale(y="independent"),
     use_container_width=True,
 )
