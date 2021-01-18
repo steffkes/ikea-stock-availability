1c7520de5fdde3a84339a9c05dc952af4200744f
steffkes <stefan@gelungen.es>
2021-01-18 14:24:26 +0100

    visualize 70340807

--- tmp-02.py
+++ tmp-02.py
@@ -35,7 +35,7 @@ def computed_stats(row):
 @st.cache
 def load_data():
     data = pd.read_json(
-        "tmp/40474214-latest.jsonl.gz",
+        "tmp/70340807-latest.jsonl.gz",
         lines=True,
         compression="gzip",
         dtype={"store_id": object, "article_id": object},
